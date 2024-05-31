b11ac462e7c14e5cb009fc061c07a2eaa65e6e5b
Gayatri Mahajan <emailofgayatrimahajan@gmail.com>
2024-05-31 21:39:18 +0530

    Star Pattern: Square

---
+++ Star Pattern/pattern_01_square_star_pattern.js
@@ -0,0 +1,18 @@
+/*
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+*/
+
+let pattern = '';
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        pattern += '* ';
+    }
+    pattern += '\n';
+}
+console.log(pattern);
