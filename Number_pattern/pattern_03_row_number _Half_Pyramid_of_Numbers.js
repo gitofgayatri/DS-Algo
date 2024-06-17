d17dd88b70eb768f723d361d25698860d97ad230
Gayatri Mahajan <emailofgayatrimahajan@gmail.com>
2024-06-17 16:09:57 +0530

    Number Pattern: Row Number Half Pyramid

---
+++ Number_pattern/pattern_03_row_number _Half_Pyramid_of_Numbers.js
@@ -0,0 +1,20 @@
+/*
+
+Row Number Half Pyramid
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+*/
+
+console.log('Row Number Half Pyramid');
+for (let i = 1; i <= 5; i++) {
+    let str = '';
+    for (let j = 1; j <= i; j++) {
+        str = str + j + ' ';
+    }
+    console.log(str);
+}
